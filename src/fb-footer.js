346ea40382bf2d2b28f6e52bf237fbcb262dbb77
Riasat Ali <riasatali4242@gmail.com>
2021-07-22 16:28:54 +0600

    first commit

---
+++ src/fb-footer.js
@@ -0,0 +1,429 @@
+import React from "react";
+const Footer = (props) => {
+	return (
+		<div
+			id="pageFooter"
+			data-referrer="page_footer"
+			data-testid="page_footer"
+		>
+			<ul
+				class="uiList localeSelectorList _2pid _509- _4ki _6-h _6-j _6-i"
+				data-nocookies="1"
+			>
+				<li>English (UK)</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://bn-in.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("bn_IN", "en_GB", "https:\/\/bn-in.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 0); return false;'
+						title="Bengali"
+					>
+						বাংলা
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://as-in.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("as_IN", "en_GB", "https:\/\/as-in.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 1); return false;'
+						title="Assamese"
+					>
+						অসমীয়া
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://hi-in.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("hi_IN", "en_GB", "https:\/\/hi-in.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 2); return false;'
+						title="Hindi"
+					>
+						हिन्दी
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://ne-np.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("ne_NP", "en_GB", "https:\/\/ne-np.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 3); return false;'
+						title="Nepali"
+					>
+						नेपाली
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://id-id.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("id_ID", "en_GB", "https:\/\/id-id.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 4); return false;'
+						title="Indonesian"
+					>
+						Bahasa Indonesia
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="rtl"
+						href="https://ar-ar.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("ar_AR", "en_GB", "https:\/\/ar-ar.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 5); return false;'
+						title="Arabic"
+					>
+						العربية
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://zh-cn.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("zh_CN", "en_GB", "https:\/\/zh-cn.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 6); return false;'
+						title="Simplified Chinese (China)"
+					>
+						中文(简体)
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://ms-my.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("ms_MY", "en_GB", "https:\/\/ms-my.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 7); return false;'
+						title="Malay"
+					>
+						Bahasa Melayu
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://es-la.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("es_LA", "en_GB", "https:\/\/es-la.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 8); return false;'
+						title="Spanish"
+					>
+						Español
+					</a>
+				</li>
+				<li>
+					<a
+						class="_sv4"
+						dir="ltr"
+						href="https://pt-br.facebook.com/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8"
+						onclick='require("IntlUtils").setCookieLocale("pt_BR", "en_GB", "https:\/\/pt-br.facebook.com\/?stype=lo&amp;jlou=AfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA&amp;smuh=53315&amp;lh=Ac_f5YVV1d5FZWK43N8", "www_list_selector", 9); return false;'
+						title="Portuguese (Brazil)"
+					>
+						Português (Brasil)
+					</a>
+				</li>
+				<li>
+					<a
+						role="button"
+						class="_42ft _4jy0 _517i _517h _51sy"
+						rel="dialog"
+						ajaxify="/settings/language/language/?uri=https%3A%2F%2Fpt-br.facebook.com%2F%3Fstype%3Dlo%26jlou%3DAfdXz02IhuNFvdVrFwzUEFUf13yspbFy5A0MsGbSLPsBtWYNDxH2pmmJLrjdojGE3w28BJ9WppT59Q80ET3XHQRv-y-6g0HJ6iE1xnTnkLexfA%26smuh%3D53315%26lh%3DAc_f5YVV1d5FZWK43N8&amp;source=www_list_selector_more"
+						href="#"
+						title="Show more languages"
+					>
+						<i class="img sp_UBPF3T_Dv7F_2x sx_32fe08"></i>
+					</a>
+				</li>
+			</ul>
+			<div id="contentCurve"></div>
+			<div
+				id="pageFooterChildren"
+				role="contentinfo"
+				aria-label="Facebook site links"
+			>
+				<ul class="uiList pageFooterLinkList _509- _4ki _703 _6-i">
+					<li>
+						<a href="/r.php" title="Sign up for Facebook">
+							Sign Up
+						</a>
+					</li>
+					<li>
+						<a href="/login/" title="Log in to Facebook">
+							Log In
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://messenger.com/"
+							title="Take a look at Messenger."
+						>
+							Messenger
+						</a>
+					</li>
+					<li>
+						<a href="/lite/" title="Facebook Lite for Android.">
+							Facebook Lite
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.facebook.com/watch/"
+							title="Browse our Watch videos."
+						>
+							{" "}
+							Watch{" "}
+						</a>
+					</li>
+					<li>
+						<a
+							href="/directory/people/"
+							title="Browse our people directory."
+						>
+							People
+						</a>
+					</li>
+					<li>
+						<a
+							href="/directory/pages/"
+							title="Browse our Pages directory."
+						>
+							Pages
+						</a>
+					</li>
+					<li>
+						<a href="/pages/category/">Page categories</a>
+					</li>
+					<li>
+						<a
+							href="/places/"
+							title="Take a look at popular places on Facebook."
+						>
+							Places
+						</a>
+					</li>
+					<li>
+						<a href="/games/" title="Check out Facebook games.">
+							Games
+						</a>
+					</li>
+					<li>
+						<a
+							href="/directory/places/"
+							title="Browse our places directory."
+						>
+							Locations
+						</a>
+					</li>
+					<li>
+						<a
+							href="/marketplace/"
+							title="Buy and sell on Facebook Marketplace."
+						>
+							Marketplace
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://pay.facebook.com/"
+							target="_blank"
+							title="Learn more about Facebook Pay"
+						>
+							Facebook Pay
+						</a>
+					</li>
+					<li>
+						<a
+							href="/directory/groups/"
+							title="Browse our Groups directory."
+						>
+							Groups
+						</a>
+					</li>
+					<li>
+						<a
+							href="/jobs/"
+							title="Apply for jobs and hire on Facebook."
+						>
+							Jobs
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://www.oculus.com/"
+							target="_blank"
+							title="Learn more about Oculus"
+						>
+							Oculus
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://portal.facebook.com/"
+							target="_blank"
+							title="Learn more about Portal from Facebook"
+						>
+							Portal
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2F&amp;h=AT38HVUlrEvzFPZmUT4iEnaI5HYK6u3UApz_iHKuq9V-W8ayE4JKugRqIb1PLzenWbFO51iWJ9yfJJbWLzPlPz3dCnfaP0HFhROVjP0BW-5oTS3vgT4rgzIpWPMJtpO1LNUTd2U2bnqwHbM9PHNo3FGGJgc"
+							title="Take a look at Instagram"
+							target="_blank"
+							rel="noopener nofollow"
+							data-lynx-mode="asynclazy"
+						>
+							Instagram
+						</a>
+					</li>
+					<li>
+						<a
+							href="/local/lists/245019872666104/"
+							title="Browse our Local Lists directory."
+						>
+							Local
+						</a>
+					</li>
+					<li>
+						<a
+							href="/fundraisers/"
+							title="Donate to worthy causes."
+						>
+							Fundraisers
+						</a>
+					</li>
+					<li>
+						<a
+							href="/biz/directory/"
+							title="Browse our Facebook Services directory."
+						>
+							Services
+						</a>
+					</li>
+					<li>
+						<a
+							href="/votinginformationcenter/?entry_point=c2l0ZQ%3D%3D"
+							title="See the Voting Information Centre"
+						>
+							Voting Information Centre
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://about.facebook.com/"
+							accesskey="8"
+							title="Read our blog, discover the resource centre and find job opportunities."
+						>
+							About
+						</a>
+					</li>
+					<li>
+						<a
+							href="/ad_campaign/landing.php?placement=pflo&amp;campaign_id=402047449186&amp;nav_source=unknown&amp;extra_1=auto"
+							title="Advertise on Facebook"
+						>
+							Create ad
+						</a>
+					</li>
+					<li>
+						<a
+							href="/pages/create/?ref_type=site_footer"
+							title="Create a Page"
+						>
+							Create Page
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://developers.facebook.com/?ref=pf"
+							title="Develop on our platform."
+						>
+							Developers
+						</a>
+					</li>
+					<li>
+						<a
+							href="/careers/?ref=pf"
+							title="Make your next career move to our brilliant company."
+						>
+							Careers
+						</a>
+					</li>
+					<li>
+						<a
+							data-nocookies="1"
+							href="/privacy/explanation"
+							title="Learn about your privacy and Facebook."
+						>
+							Privacy
+						</a>
+					</li>
+					<li>
+						<a
+							href="/policies/cookies/"
+							title="Learn about cookies and Facebook."
+							data-nocookies="1"
+						>
+							Cookies
+						</a>
+					</li>
+					<li>
+						<a
+							class="_41ug"
+							data-nocookies="1"
+							href="https://www.facebook.com/help/568137493302217"
+							title="Learn about AdChoices."
+						>
+							AdChoices
+							<i class="img sp_UBPF3T_Dv7F_2x sx_f4d3d6"></i>
+						</a>
+					</li>
+					<li>
+						<a
+							data-nocookies="1"
+							href="/policies?ref=pf"
+							accesskey="9"
+							title="Review our terms and policies."
+						>
+							Terms
+						</a>
+					</li>
+					<li>
+						<a
+							href="/help/?ref=pf"
+							accesskey="0"
+							title="Visit our Help Centre."
+						>
+							Help
+						</a>
+					</li>
+					<li>
+						<a
+							accesskey="6"
+							class="accessible_elem"
+							href="/settings"
+							title="View and edit your Facebook settings."
+						>
+							Settings
+						</a>
+					</li>
+					<li>
+						<a
+							accesskey="7"
+							class="accessible_elem"
+							href="/allactivity?privacy_source=activity_log_top_menu"
+							title="View your activity log"
+						>
+							Activity log
+						</a>
+					</li>
+				</ul>
+			</div>
+			<div class="mvl copyright">
+				<div>
+					<span> Facebook © 2021</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Footer;
